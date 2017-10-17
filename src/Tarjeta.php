b73d38ce3d1099b806b6b2c179854660488d5913
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-17 17:29:35 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,7 +4,30 @@ namespace TpFinal;
 
 class Tarjeta {
 
+    public $sa;
+    
     public function saldo() {
-        return 0;
+        return $this->sa;
     }
+    
+    public function cargar($p){
+        if ($p==272){
+            $this->sa=$this->sa+320;
+        }
+        elseif($p==332){
+            $this->sa=$this->sa+388;
+        }
+        elseif($p==500){
+            $this->sa=$this->sa+640;
+        }
+        else{
+            $this->sa=$this->sa+$p;
+        }
+    }
+    
+    public function pagar($veh,$hor){
+    
+    }
+    
+    
 }
