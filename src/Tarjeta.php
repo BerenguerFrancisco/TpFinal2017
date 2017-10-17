7bb7325741760f7a197e1984a6ffb48f96f334cd
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-17 17:49:17 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -5,6 +5,7 @@ namespace TpFinal;
 class Tarjeta {
 
     public $sa;
+    public $vp;
     
     public function saldo() {
         return $this->sa;
@@ -23,10 +24,24 @@ class Tarjeta {
         else{
             $this->sa=$this->sa+$p;
         }
+        
+        if ($this->vp==1){
+            $this->sa=$this->sa-9.75;
+        }
+        if ($this->vp==2){
+            $this->sa=$this->sa-(9.75*2);
+        }
     }
     
     public function pagar($veh,$hor){
-    
+        if ($this->sa<9.75){
+            if($this->vp<2){
+                $this->vp=$this->vp+1;
+            }
+            else
+            {echo "No tenes más platula ni más viajes plus papu, bajate del colectivo y despedite de tu cuenta\n";
+            }
+           
     }
     
     
