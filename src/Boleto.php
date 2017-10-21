e9fdd17cbafee3ab3d912bbe0a2c716c064d5f01
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-20 21:25:40 -0300

    Update Boleto.php

--- src/Boleto.php
+++ src/Boleto.php
@@ -1,6 +1,7 @@
 <?php
 namespace TpFinal;
 class boleto {
+    
     public $fecha;
     public $tipo;
     public $saldo;
@@ -8,11 +9,22 @@ class boleto {
     public $id;
     
     public function __construct ($f,$t,$s,$l,$i) {
-    $this->fecha=$f;
-    $this->tipo=$t;
-    $this->saldo=$s;
-    $this->linea=$l;
-    $this->id=$i;
+        
+        $this->fecha=$f;
+        $this->tipo=$t;
+        $this->saldo=$s;
+        $this->linea=$l;
+        $this->id=$i;
     }
+    public function mostrarDatos {
+        
+        echo "<li> Fecha: ".$this->fecha;
+        echo "<li> Viaje ".$this->tipo;
+        echo "<li> Saldo: ".$this->saldo;
+        echo "<li> Linea: ".$this->linea;
+        echo "<li> Id tarjeta: ".$this->id;
+    } 
     
+    
+    }
 }
