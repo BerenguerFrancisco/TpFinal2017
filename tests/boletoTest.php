11bc50dd1e2bcacdbd001a6eeb80cbc7b90fe41b
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-20 21:41:16 -0300

    Update boletoTest.php

--- tests/boletoTest.php
+++ tests/boletoTest.php
@@ -6,7 +6,7 @@ class EstacionTest extends TestCase {
      * Comprueba que los datos del boleto sean correcto
      */
     public function testTipoCorrecto() {
-        $boleto = new boleto;
-        $this->assertContains($boleto->tipo, ["Normal, Plus, Medio"]);
+        $boleto = new boleto('11/04/17','Plus',200,'144 Negro',1202010);
+        $this->assertContains($boleto->tipo, ['Normal', 'Plus', 'Medio']);
     }
 }
