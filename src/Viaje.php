455c5504756a938b853229cc6a35758e6a8d952a
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-28 17:33:42 -0300

    Update Viaje.php

--- src/Viaje.php
+++ src/Viaje.php
@@ -5,15 +5,25 @@ class viaje {
     protected $tipo;
     protected $idtarjeta;
     protected $transporte;
+    
     public function __construct ($t,$it,$trans) {
         
         $this->tipo=$t;
-        $this->empresa=$e;
+        $this->idtarjeta=$it;
+        $this->transporte=$trans
+    }
+    public function devolverTipo() {
+    
+    
+    }
+    public function devolverMonto() {
+    }
+    public function devolverTransporte() {
     }
  
 
 }
- $viaje tipo(); // Devuelve en colectivo →
- $viaje monto(); // Devuelve el monto del viaje →
- $viaje tranporte()->nombre(); // Devuelve el nombre de la linea o de la bici →
+ tipo(); // Devuelve en colectivo →
+monto(); // Devuelve el monto del viaje →
+ tranporte()->nombre(); // Devuelve el nombre de la linea o de la bici →
 que se utilizó
