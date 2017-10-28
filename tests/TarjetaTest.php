25617c2fc401ccbce1e4cec502dfae783e2fd09b
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-28 20:43:55 -0300

    Update TarjetaTest.php

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -13,4 +13,23 @@ class EstacionTest extends TestCase {
         $tarjeta = new Tarjeta;
         $this->assertEquals($tarjeta->saldo(), 0);
     }
+    
+    public function carga50() {
+        $tarjeta= new Tarjeta;
+        $tarjeta->cargar(50);
+        $this->assertEqueals($tarjeta->saldo(),50);
+    }
+    public function carga332() {
+        $tarjeta= new Tarjeta;
+        $tarjeta->cargar(332)
+        $this->assertEquals($tarjeta->saldo(),388);
+    }
+    
+    public function viaje() {
+        $tarjeta= new Tarjeta;
+        $tarjeta->cargar(50);
+        $sal=$tarjeta->saldo();
+        $bondi= new Colectivo(144,"Rosariobus");
+        $tarjeta->pagar($bondi, "28/10/17 20:35");
+        $this->assertEquals($tarjeta->saldo(),$sal-9.75);
 }
