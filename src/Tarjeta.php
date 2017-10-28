bf08679689693d3d2afe4b170641623049008ff0
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-28 17:36:26 -0300

    Update Tarjeta.php

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -14,33 +14,18 @@ class tarjeta {
     public function cargar($p){
         if ($p==272){
             $this->sa=$this->sa+320;
-             $this->vp=0;
         }
         elseif($p==332){
             $this->sa=$this->sa+388;
-             $this->vp=0;
         }
         elseif($p==500){
             $this->sa=$this->sa+640;
-             $this->vp=0;
+        }
+        elseif($p==624){
+            $this->sa=$this->sa+776;
         }
         else{
             $this->sa=$this->sa+$p;
-             if ($this->vp==1 && $p>=10){
-             $this->vp=0;
-             }
-            ifelse($this->vp==2 && $p>=20){
-            $this->vp=0;
-            }
-        }
-        
-        if ($this->vp==1){
-            $this->sa=$this->sa-9.75;
-            $this->vp=0;
-        }
-        if ($this->vp==2){
-            $this->sa=$this->sa-(9.75*2);
-            $this->vp=0; 
         }
     }
     
@@ -58,6 +43,14 @@ class tarjeta {
         { if ($veh instanceof bici)
             {if 
           $this->sa=$this->sa-9.75;
+        if ($this->vp==1){
+            $this->sa=$this->sa-9.75;
+            $this->vp=0;
+        }
+        if ($this->vp==2){
+            $this->sa=$this->sa-(9.75*2);
+            $this->vp=0; 
+        }
         }
     
     
