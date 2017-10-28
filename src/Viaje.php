96fd241e6935ccbe7e92ca91b33c5f50f8b7d040
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-28 17:51:52 -0300

    Update Viaje.php

--- src/Viaje.php
+++ src/Viaje.php
@@ -5,12 +5,14 @@ class viaje {
     protected $tipo;
     protected $idtarjeta;
     protected $transporte;
+    protected $fecha;
     
-    public function __construct ($t,$it,$trans) {
+    public function __construct ($t,$it,$trans,$f) {
         
         $this->tipo=$t;
         $this->idtarjeta=$it;
         $this->transporte=$trans;
+        $this->fecha=$f;
     }
     
     public function devolverTipo() {
@@ -25,6 +27,9 @@ class viaje {
     
     public function devolverTransporte() {
           return $this->transporte;
+    }
+    public function devolverTransporte() {
+          return $this->fecha;
     }
 }
 
