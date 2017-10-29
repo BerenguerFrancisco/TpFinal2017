9b0b2d2e4b1cba7e8a82c5db2f1836b884b6b364
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-28 21:34:07 -0300

    Update colectivoTest.php

--- tests/colectivoTest.php
+++ tests/colectivoTest.php
@@ -58,7 +58,8 @@ class colectivoTest extends TestCase {
      * Comprueba que los datos del colectivo sean correcto
      */
     public function testLineaCorrecta() {
-        $bondi = new colectivo('144');
+        $bondi = new colectivo('144','Rosario BUS);
         $this->assertContains($bondi->linea,$lineas);
+        $this->assertContains($bondi->empresa,('Rosario BUS','EMTR');
     }
 }
