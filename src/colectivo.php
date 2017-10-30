342d26b740702eacf17c0a01d3901c9868c0a1d8
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-30 01:38:21 -0300

    Update colectivo.php

--- src/colectivo.php
+++ src/colectivo.php
@@ -10,7 +10,7 @@ class colectivo {
         $this->linea=$l;
         $this->empresa=$e;
     }
-    public function imprimirLinea {
+    public function imprimirLinea() {
         echo "<li> Linea: ".$this->linea;
 
     } 
