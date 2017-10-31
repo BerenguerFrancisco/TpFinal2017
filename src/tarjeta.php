1cb7d97b17d706ea05a7f36b7db919452da46259
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-31 00:00:05 -0300

    Update tarjeta.php

--- src/tarjeta.php
+++ src/tarjeta.php
@@ -33,13 +33,15 @@ class tarjeta {
         }
     }
     
-    public function pagar($veh,$hor){
+    public function pagar($veh,$fec){
+        $fec = strtotime($fec);
         if ($veh instanceof bici)
             {if ($this->sa<9.75){
                 echo "No tenes más platula ni más viajes plus papu, bajate del colectivo y despedite de tu cuenta\n";
             }
             if ($this->sa>9.75){
-                 $this->sa=$this->sa-9.75;
+                $this->sa=$this->sa-9.75;
+                array_unshift(($this->viajesrealizados), new viaje($fec, "Normal", "Bici", $this->id));
             }
         }
         elseif ($veh instanceof colectivo){
@@ -52,16 +54,16 @@ class tarjeta {
                 $this->sa=$this->sa-(9.75*2);
                 $this->vp=0; 
                 }
-            array_unshift(($this->viajesrealizados), new viaje($hor, "normal", "Colectivo", $this->id));
-            $bol = new boleto("23:23", "normal", $this->sa, $veh->linea, $this->id);
+            array_unshift(($this->viajesrealizados), new viaje($fec, "normal", "Colectivo", $this->id));
+            $bol = new boleto($fec, "normal", $this->sa, $veh->linea, $this->id);
             $bol->imprimirboleto();    
             }
             if($this->sa<9.75){
                 $this->sa=$this->sa-9.75;
                 if ($this->vp!=2){
                     $this->vp=$this->vp+1;       
-                    array_unshift(($this->viajesrealizados), new viaje($hor, "plus", "Colectivo", $this->id));
-                    $bol = new boleto("23:23", "normal", $this->sa, $veh->linea, $this->id);
+                    array_unshift(($this->viajesrealizados), new viaje($fec, "plus", "Colectivo", $this->id));
+                    $bol = new boleto($fec, "normal", $this->sa, $veh->linea, $this->id);
                     $bol->imprimirboleto(); 
                 }
                 else{
