171b0ba7c9cc1982261760f7fd2fa566607c4825
julianesteveb <31454104+julianesteveb@users.noreply.github.com>
2017-10-31 17:11:09 -0300

    Update viajeTest.php

--- tests/viajeTest.php
+++ tests/viajeTest.php
@@ -5,11 +5,13 @@ use PHPUnit\Framework\TestCase;
 
 class viajeTest extends TestCase {
     public function testTipoCorrecto() {
-        $tipos=['Normal', 'Plus', 'Medio','Trasbordo','Medio Trasbordo','Gratis'];
+        $tipos=['Normal', 'Plus','Trasbordo','Gratis'];
         
         $bondi = new colectivo('144','Rosario BUS');
         $tarjeta= new tarjeta("1564");
         $viaje= new viaje('Normal',$tarjeta,$bondi,'31/10/17 12:02');
+        $viaje2= new viaje('Transbordo',$tarjeta,$bondi,'31/10/17 12:02');
+        
         
         $this->assertContains($viaje->devolverTipo(),$tipos);
         
@@ -17,6 +19,8 @@ class viajeTest extends TestCase {
         $this->assertEquals($viaje->devolverTransporte()->empresa,$bondi->empresa);
         
         $this->assertEquals($viaje->devolverMonto(),9.75);
+        $this->assertEquals($viaje2->devolverMonto(),3.20);
+
         $this->assertEquals($viaje->devolverFecha(),'31/10/17 12:02');
         
         $this->assertEquals($viaje->idTarjeta,$tarjeta->id);
