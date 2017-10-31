044bafdefebdf90f4bab76d9e3711623a6518404
BerenguerFrancisco <32310105+BerenguerFrancisco@users.noreply.github.com>
2017-10-30 23:25:05 -0300

    Update tarjeta.php

--- src/tarjeta.php
+++ src/tarjeta.php
@@ -53,15 +53,17 @@ class tarjeta {
                 $this->sa=$this->sa-(9.75*2);
                 $this->vp=0; 
                 }
-            $this->bol=$this->bol+1;
-            $this->viajesrealizados($this->bol)=new boleto($hor, "normal", $this->sa, $veh->linea, $this->id);    
+            $this->viaj=$this->viaj+1;
+            $this->viajesrealizados[$this->viaj]=new viaje($hor, "normal", "Colectivo", $this->id);
+            $bol = new boleto("23:23", "normal", $this->sa, $veh->linea, $this->id);
+            $bol->imprimirboleto();    
             }
             if($this->sa<9.75){
                 $this->sa=$this->sa-9.75;
                 if ($this->vp!=2){
                     $this->vp=$this->vp+1;       
-                    $this->bol=$this->bol+1;
-                    $this->viajesrealizados($this->bol)=new boleto($hor, "plus", $this->sa, $veh->linea, $this->id);}
+                    $this->viaj=$this->viaj+1;
+                    $this->viajesrealizados[$this->viaj]=new viaje($hor, "plus", "Colectivo", $this->id); }
                 else{
                     echo "No tiene saldo ni viajes plus";    
                 }
